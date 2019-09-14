dc7ffb75a0dfc3cd41bfedab50ef4c582ed7170a
Chema Gonzalez <chemag@fb.com>
2019-09-13 18:43:10 -0700

    sherlock: add long options parsing

--- sherlock265/sherlock265.cc
+++ sherlock265/sherlock265.cc
@@ -24,12 +24,116 @@
   SOFTWARE.
 */
 
+#include <getopt.h>
 #include "VideoPlayer.hh"
 
 
+typedef struct arg_values {
+  char input_filename[1024];
+  int debug;
+  int nrem;
+  char** rem;
+} arg_values;
+
+static arg_values values;
+
+#define DEFAULT_DEBUG 0
+#define DEFAULT_INPUT_FILENAME ""
+
+static struct option longopts[] = {
+    // options with no argument
+    {"debug", no_argument, &values.debug, values.debug+1},
+    {"quiet", no_argument, &values.debug, 0},
+    // matching options to short options
+    {"input", required_argument, NULL, 'i'},
+    {"output", required_argument, NULL, 'o'},
+    {NULL, 0, NULL, 0}
+};
+
+void usage(char *name)
+{
+  fprintf(stderr,"usage: sherlock265 [options] videofile.bin\n");
+  fprintf(stderr,"where:\n");
+  fprintf(stderr,"  -d: increase debug information\n");
+  fprintf(stderr,"  -q: quiet (no debug information\n");
+  fprintf(stderr,"  --tint-qp: tint QP information\n");
+  fprintf(stderr,"videofile.bin: a raw h.265 bitstream (e.g. HM-10.0 output)\n");
+}
+
+arg_values *parse_args (int argc, char** argv) {
+  strcpy(values.input_filename, DEFAULT_INPUT_FILENAME);
+  values.debug = DEFAULT_DEBUG;
+
+  // getopt_long stores the option index here
+  int optindex = 0;
+  // long options
+
+  static struct option longopts[] = {
+      // options with no argument
+      {"debug", no_argument, &values.debug, values.debug+1},
+      {"quiet", no_argument, &values.debug, 0},
+      {NULL, 0, NULL, 0}
+  };
+
+  int c;
+  while ((c = getopt_long(argc, argv, "dqh?", longopts, &optindex)) != -1)
+  {
+    switch (c)
+    {
+      case 0:
+        // long options that define flag
+        if (longopts[optindex].flag != NULL) {
+          break;
+        }
+        printf ("option %s", longopts[optindex].name);
+        if (optarg) {
+          printf (" with arg %s", optarg);
+        }
+        printf ("\n");
+        break;
+
+      case 'd':
+        values.debug += 1;
+        break;
+
+      case 'q':
+        values.debug = 0;
+        break;
+
+      case 'h':
+      case '?':
+      default:
+        usage(argv[0]);
+        exit(0);
+        break;
+
+    }
+  }
+
+  // parse non-option arguments
+  if (argc > optind) {
+    strncpy(values.input_filename, argv[optind], 1023);
+    ++optind;
+  }
+
+  // store remaining arguments
+  values.nrem = argc - optind;
+  values.rem = argv + optind;
+
+  return &values;
+}
+
 int main(int argc, char **argv)
 {
-  if (argc != 2) {
+
+  arg_values *values;
+  values = parse_args(argc, argv);
+  if (values == NULL) {
+    usage(argv[0]);
+    exit(-1);
+  }
+
+  if (values->nrem != 0) {
     fprintf(stderr,"usage: sherlock265 videofile.bin\n");
     fprintf(stderr,"The video file must be a raw h.265 bitstream (e.g. HM-10.0 output)\n");
     exit(5);
@@ -38,7 +142,7 @@ int main(int argc, char **argv)
 
   QApplication app(argc, argv);
 
-  VideoPlayer videoPlayer(argv[1]);
+  VideoPlayer videoPlayer(values->input_filename);
   videoPlayer.show();
 
   return app.exec();
